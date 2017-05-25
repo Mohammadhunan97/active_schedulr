92277665e6f2b6cf167a6a571abe51f6cdbe1087
Mohammad Chughtai <mohammadhunan@gmail.com>
2017-05-25 19:05:57 -0400

    delete of crud

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,9 +6,8 @@ class EventsController < ApplicationController
 			redirect_to "/users/sign_in"
 		end
 	end
-	
+
 	def create
-		puts params
 		Event.create(
 			date: params["date"],
 			title: params["title"],
@@ -16,13 +15,9 @@ class EventsController < ApplicationController
 			user_id: current_user.id
 			)
 	end
+
+	def destroy
+		Event.destroy(params["id"]);
+	end
 end
 
-
- # Book.create(title: params[:title],
- #      author: params[:authors],
- #      isbn: params[:isbn],
- #      description: params[:description],
- #      genre: params[:genre],
- #      user_id: current_user.id,
- #      status: "Shelfed")
